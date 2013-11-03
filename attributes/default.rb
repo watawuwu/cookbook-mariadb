c5db54c71b0bae3147882f4728947027bf4da692
watawu <watawu.m@gmail.com>
2013-11-03 20:40:16 +0900

    Valided my.cnf template.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,22 @@
 default['mariadb']['version'] = "10.0"
-# for slow query option
+
+# For slow query options.
 default['mysql']['version'] = "5.6"
 
 # MariaDB doesn't use upstart like vanilla MySQL does.
 normal['mysql']['use_upstart'] = false
 
+# Added tunable options
+normal['mysql']['tunable']['myisam_sort_buffer_size'] = "512M"
+normal['mysql']['tunable']['general_log'] = "/var/log/mysql/mysql.log"
+normal['mysql']['tunable']['slow_query_log'] = "/var/log/mysql/slow.log"
+normal['mysql']['tunable']['log_slow_verbosity'] = "query_plan"
+normal['mysql']['tunable']['log_slow_admin_statements'] = 1
+normal['mysql']['tunable']['report_host'] = "master1"
+normal['mysql']['tunable']['log_bin_index'] = "/var/log/mysql/mariadb-bin.index"
+normal['mysql']['tunable']['relay_log_info_file'] = "/var/log/mysql/relay-bin.info"
+normal['mysql']['tunable']['default_storage_engine'] = "InnoDB"
+
+# Modified package
 normal['mysql']['client']['packages'] = %W{mariadb-client libmariadbclient-dev}
 normal['mysql']['server']['packages'] = %W{mariadb-server}
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,4 +1,11 @@
 include_recipe "mariadb::mariadb_repo"
 include_recipe "mysql::server"
 
-
+# override my.cnf
+begin
+  t = resources(:template => "#{node['mysql']['conf_dir']}/my.cnf")
+  t.source "my.cnf.erb"
+  t.cookbook "mariadb"
+rescue Chef::Exceptions::ResourceNotFound
+  Chef::Log.warn "could not find template #{node['mysql']['conf_dir']}/my.cnf to modify"
+end
